90694083af3a000597c5dd36a1440c80c04ba1ae
Furkan  kızıltoprak <kiziltoprakfurkan001@gmail.com>
2024-04-16 17:27:24 +0300

    merhabalar

---
+++ src/DosyaOlusturma.java
@@ -0,0 +1,5 @@
+public class DosyaOlusturma {
+    public static void main(String[] args) {
+
+    }
+}
--- src/Olusturma.java
+++
@@ -1,7 +0,0 @@
-import java.io.File;
-
-public class Olusturma {
-    public static void main(String[] args) {
-        System.out.println("furkan");
-    }
-}
