bf013cb8eb16ece450905bcd9d1aa7cc091037df
Furkan  kızıltoprak <kiziltoprakfurkan001@gmail.com>
2024-04-16 17:31:02 +0300

    Dosya oluşturma.

--- src/DosyaOlusturma.java
+++ src/DosyaOlusturma.java
@@ -1,5 +1,19 @@
+import java.io.File;
+import java.io.IOException;
+
 public class DosyaOlusturma {
     public static void main(String[] args) {
+        File dosya = new File("ornekdosya.txt");
+        try {
+            if (!dosya.exists()){
+                dosya.createNewFile();
+                System.out.println("Dosya başarıyla oluşturuldu.");
+            }else {
+                System.out.println("Dosya zaten var.");
+            }
 
+        }catch (IOException e ){
+            System.out.println("Bir hata oluştu.");
+        }
     }
 }
